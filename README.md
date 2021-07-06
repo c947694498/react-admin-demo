c88ff5909914913e1904cad0343f805d3df18fda
cbb <947694498@qq.com>
2021-07-06 14:08:13 +0800

    chroe: add craco

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 ## plan
 [] router 添加嵌套层级
 [] router 拦截 https://segmentfault.com/a/1190000022259376
+[] 添加craco，对create-react-app自定义配置支持
 [] 引入ant design
 [] style 引入方式
 [] 了解css Modules
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,18 +1,25 @@
 import { lazy } from "react"
 
 const routes = [
-
+  {
+    path: '/Login',
+    component: lazy(() => import('../pages/Login/Login'))
+  },
+  {
+    path: '/Register',
+    component: lazy(() => import('../pages/Register/Register')),
+  },
   {
     path: '/Home',
-    component: lazy(() => import('../pages/Home'))
+    component: lazy(() => import('../pages/Home/Home'))
   },
   {
     path: '/My',
-    component: lazy(() => import('../pages/My')),
+    component: lazy(() => import('../pages/My/My')),
   },
   {
     path: '*',
-    component: lazy(() => import('../pages/NotFound'))
+    component: lazy(() => import('../pages/NotFound/NotFound'))
   }
 ]
 
